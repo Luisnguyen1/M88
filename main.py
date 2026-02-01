0385511510cecd012fef46cab98f26fd97dcaf2e
Your Name <you@example.com>
2026-02-01 11:38:32 +0700

    thêm api register

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+taikhoan = []
 
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"message": "Hello, World!"})
+
+@app.route('/register', methods=['POST'])
+def register():
+    user_name = request.json.get('username')
+    password = request.json.get('password')
+
+    
+    # Thêm username và password vào mảng tài khoản
+    if user_name and password:
+        taikhoan.append({
+            'username': user_name,
+            'password': password,
+            'spins': 1
+        })
+        return jsonify({
+            "message": "User registered successfully!",
+            "accounts": taikhoan
+        })
+    else:
+        return jsonify({"message": "Username and password are required!"}), 400
+    
 
 app.run()
