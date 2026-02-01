78b8260af7cf17b8795251cde697f6323a25bf4d
crayzybenwofl <166887729+crayzybenwofl@users.noreply.github.com>
2026-02-01 11:45:03 +0700

    random+add

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
-
+gifts = [
+    {
+        "name": "Teddy Bear",
+        "price": 29.99,
+        "ULRImage":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_KfcishZysQ2FS-ryOUqW7BJomtsbw_jhcQ&s"
+    },
+    {
+        "name": "Lego Set",
+        "price": 59.99,
+        "ULRImage":"https://www.lego.com/cdn/cs/set/assets/blt52d7fce5233e7f7f/76300_Prod_en-gb.png?fit=crop&quality=80&width=400&height=400&dpr=1"
+    }
+]
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"message": "Hello, World!"})
-
+@app.route('/random-gifts', methods=['GET'])
+def random_gifts():
+    random_gift = random.choices(gifts, weights=[gift["price"] for gift in gifts])
+    return jsonify(random_gift)
+@app.route('/add-gift/<gift_name>,<gift_price>,<gift_image>', methods=['POST'])
+def add_gift(gift_name, gift_price, gift_image):
+    if(not gift_name or not gift_price or not gift_image):
+        return jsonify({"error": "All fields (name, price, image URL) are required."}), 400
+    new_gift = {
+        "name": gift_name,
+        "price": float(gift_price),
+        "ULRImage": gift_image
+    }
+    gifts.append(new_gift)
+    return jsonify({"message": f"Gift '{gift_name}' added successfully!"})
 app.run()
